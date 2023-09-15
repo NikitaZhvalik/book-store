0dbe1f2244ba306cee64f7ea67c2a28b157c2724
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-09-16 00:46:46 +1000

    v.5

--- src/toolkitRedux/booksReducer.js
+++ src/toolkitRedux/booksReducer.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 const initialState = {
 	books: [],
-    isLoading: false
+    isLoading: false,
+    error: null
 }
 
 const key = "AIzaSyCd535-0dsJ9kCIUPlVYnahGx0esWYFhTA"
@@ -11,11 +12,17 @@ const key = "AIzaSyCd535-0dsJ9kCIUPlVYnahGx0esWYFhTA"
 export const getBooks = createAsyncThunk(
     'books/getBooks',
     async ({query, filterCategories, filterNewest, startIndex, maxResults}, {rejectWithValue, dispatch}) => {
-        dispatch(setLoading(true))
-        const server = `https://www.googleapis.com/books/v1/volumes?q=${query}+terms${filterCategories === "all" ? '' : "+subject" + filterCategories}:&orderBy=${filterNewest}&startIndex=${startIndex}&maxResults=${maxResults}&key=${key}`
-        const res = await axios.get(server)
-        dispatch(setBooks(res?.data))
-        dispatch(setLoading(false))
+        try {
+            dispatch(setLoading(true))
+            const server = `https://www.googleapis.com/books/v1/volumes?q=${query}+terms${filterCategories === "all" ? '' : "+subject" + filterCategories}:&orderBy=${filterNewest}&startIndex=${startIndex}&maxResults=${maxResults}&key=${key}`
+            const res = await axios.get(server)
+            dispatch(setBooks(res?.data))
+            dispatch(setLoading(false))
+        } catch (error) {
+            console.log(error.response.data.error.message)
+            alert(error.response.data.error.message)
+            return rejectWithValue(error.response.data)
+        }
     }
 )
 
@@ -28,14 +35,20 @@ export const booksSlice = createSlice({
         },
         setLoading: (state, action) => {
             state.isLoading = action.payload
+        },
+        setError: (state, action) => {
+            state.error = action.payload
         }
     },
     extraReducers: {
         [getBooks.fulfilled]: () => console.log('fulfilled'),
         [getBooks.pending]: () => console.log('pending'),
-        [getBooks.rejected]: () => alert('server error'),
+        [getBooks.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
     }
 })
 
-export const {setBooks, setLoading} = booksSlice.actions
+export const {setBooks, setLoading, setError} = booksSlice.actions
 export default booksSlice.reducer
